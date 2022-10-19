2658caa3aa7ed3637a8ba1084b795818ab7afe71
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-10-20 03:24:33 +0600

    added category wise news load and single news load by using dynamic API

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./Data/Categories.json')
+const categories = require('./Data/Categories.json');
+const news = require('./Data/News.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
@@ -13,6 +14,31 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.params.id)
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const selectedCategoryNews = news.filter(n => n.category_id === id);
+        res.send(selectedCategoryNews);
+    }
+
+
 })
 
 app.listen(port, () => {
